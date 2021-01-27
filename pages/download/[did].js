e10f0ff7de9dea85ca8148010bce3eb255202d95
Rick Smit <rick.p.smit@gmail.com>
2021-01-27 13:14:51 +0100

    dont use namespaced import :sweat:

--- pages/download/[did].js
+++ pages/download/[did].js
@@ -1,14 +1,14 @@
 import Head from "next/head";
 
 const Dog = ({ dog, breed }) => (
-  <React.Fragment>
+  <>
     <Head>
       <title>{breed}</title>
       <meta property="og:image" content={dog} />
     </Head>
     <p>Breed: {breed}</p>
     <img src={dog} />
-  </React.Fragment>
+  </>
 );
 
 Dog.getInitialProps = async ({ query }) => {
